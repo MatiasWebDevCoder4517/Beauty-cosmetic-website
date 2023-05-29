aece8a502fec12bfeba4b55d462503ae2d4cdcfc
Matias <matias.gonzalez@tecnoandina.cl>
2023-05-29 18:53:56 -0400

    Improving fixes and visual

--- app/assests/js/style.js
+++ app/assests/js/style.js
@@ -2,11 +2,9 @@
 const showMneu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
-  /*valida se se existe as variaveis*/
+
   if (toggle && nav) {
-    /*aqui adiciono a class show-menu na div com a class nav__menu*/
     toggle.addEventListener("click", () => {
-      /*adicina a class show-menu na div que tem o id nav-menu */
       nav.classList.toggle("show-menu");
     });
   }
@@ -17,12 +15,11 @@ showMneu("nav-toggle", "nav");
 const navLink = document.querySelectorAll(".nav__link");
 function linkAction() {
   const navMenu = document.getElementById("nav");
-  /*Quando um link ou o botão close for clicado a class show-menu será removido*/
+
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*==================== DEIXA O LINK CLICADO COM A CLASS ACTIVE-LINK ====================*/
 const linkColor = document.querySelectorAll(".nav__link");
 
 function colorLink() {
@@ -33,7 +30,7 @@ function colorLink() {
 }
 
 linkColor.forEach((L) => L.addEventListener("click", colorLink));
-/*==================== MUDAR A COR DO HEADER ====================*/
+
 function scrollHeader() {
   const scrollHeader = document.getElementById("header");
   if (this.scrollY >= 100) {
